d45ded57d330ea7518c332f1c53f8ffd659ec84f
Danilo Diniz <danilosdinizz@gmail.com>
2021-07-09 22:55:56 -0300

    .

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Home from './pages/Home/index';
+import Reservas from './pages/Reservas';
+
+export default function Routes(){
+    return(
+        <Switch>
+            <Route  path="/" exact component={Home}/>
+            <Route  path="/reservas" exact component={Reservas}/>
+        </Switch>
+    )
+}
