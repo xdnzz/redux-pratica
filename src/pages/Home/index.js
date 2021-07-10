333597f7442e3f561800565ff42c5481d01d6058
Danilo Diniz <danilosdinizz@gmail.com>
2021-07-10 17:30:06 -0300

    components stylizeds

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import api from  '../../services/api'
+import './style.css'
+import {MdFlightTakeoff} from 'react-icons/md'
 
 export default function Home() {
 
@@ -11,7 +13,6 @@ async function loadApi(){
   const response = await api.get('trips');
   setTrips(response.data)
 
-  console.log(response.data)
 }
 
 loadApi();
@@ -21,9 +22,21 @@ loadApi();
 
  return (
    <div>
-       <h1>
-           Home
-       </h1>
+       <div className="box"> 
+          {trips.map(trip => (
+            <li key={trip.id}>
+              <img src={trip.image} alt={trip.title} />
+              <strong>{trip.title}</strong>
+              <button type="button" onClick={()=>{}}>
+              <div>
+                <MdFlightTakeoff syze={16} color="FFF"/>
+              </div>
+              <span> Status: {trip.status ? 'Disponível' : 'Indisponível'}</span>
+
+              Solicitar reserva</button>
+            </li>
+          ))}
+       </div>
    </div>
  );
 }
