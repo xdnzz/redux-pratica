7e44f1d49dd32b7a472dacb0028864d938d1e859
Danilo Diniz <danilosdinizz@gmail.com>
2021-07-10 15:15:54 -0300

    .

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
+import logo from '../../assets/logo.svg'
+import './style.css'
 
 export default function Header(){
     return(
-        <header>
-            <h1>Header</h1>
+        
+        <header className="container">
+            <Link to="/">
+            <img className="logo" src={logo} alt="logo"/>
+            </Link>
+
+            <Link className="reserva" to="/reservas">
+                <div>
+                    <strong>Minhas Reservas</strong>
+                    <span>0 reservas</span>
+                </div>
+            </Link>
+          
         </header>
     )
 }
