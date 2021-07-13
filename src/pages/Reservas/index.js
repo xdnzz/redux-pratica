c071cf166c8c585f6e9f1161630f1b6b90c66995
Danilo Diniz <danilosdinizz@gmail.com>
2021-07-13 19:36:13 -0300

    dependencias instaladas

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './style.css'
+import {MdDelete} from 'react-icons/md'
+
 
 export default function Reservas() {
  return (
    <div>
-       <h1>Reservas</h1>
-   </div>
- );
+       <h1 className="title">Você solicitou 1 reserva</h1>
+       <div className="reservas">
+          <img src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"/>
+      
+       <strong>Viagem Maceio 7 dias</strong>
+       <span>Quantidade: 2 </span>
+       <button type="button" onClick={()=>{}}> 
+        <MdDelete syze={20} color="#191919"/>
+        </button>
+
+        </div>
+        <footer>
+          <button type="button">Solicitar Reservas</button>
+        </footer>
+</div>
+
+
+
+   );
 }
